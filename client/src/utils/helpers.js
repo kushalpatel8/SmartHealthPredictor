1731cf6bffd255da84f504243e91a5f43a6a9c53
Kushal Patel <161633671+kushalpatel8@users.noreply.github.com>
2026-09-11 16:42:28 +0530

    utils

---
+++ client/src/utils/helpers.js
@@ -0,0 +1,13 @@
+export const formatDate = (dateString) => {
+    if (!dateString) return '';
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat('en-US', {
+        year: 'numeric',
+        month: 'short',
+        day: 'numeric'
+    }).format(date);
+};
+
+export const classNames = (...classes) => {
+    return classes.filter(Boolean).join(' ');
+};
